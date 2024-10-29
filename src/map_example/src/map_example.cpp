39ceeeb136e6f3df4f8a2f3344024ec8ad1b55b5
phapeee <phapeeee@gmail.com>
2024-10-29 18:36:40 +0000

    updated mining_map

--- src/map_example/src/map_example.cpp
+++ src/map_example/src/map_example.cpp
@@ -1,34 +1,110 @@
 #include <ros/ros.h>
+#include <tf/transform_broadcaster.h>
+#include <tf/transform_listener.h>
+#include <tf/tf.h>
 #include <mining_map/Map.h>
+#include <geometry_msgs/Pose.h>
+#include <cmath>
 
-map::Map ieee_map(0);
+//map::Map ieee_map(0);
 map::Map ieee_caution(1);
 
+
+void botCallBack(const geometry_msgs::Pose& pose){
+        static tf::TransformBroadcaster br;
+        tf::Transform transform;
+        transform.setOrigin(tf::Vector3(pose.position.x, pose.position.y, 0));
+        transform.setRotation(tf::Quaternion(pose.orientation.x, pose.orientation.y, pose.orientation.z,  pose.orientation.w));
+        br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "world_map", "sensor"));
+}
+
+void updateBotCallback(const geometry_msgs::Pose &pose){
+	ieee_caution.updateBot(pose);
+}
+
+void dest_action_0(bool& started, bool& done){
+	static ros::Time timer;
+	if (!started && !done){
+		started = true;
+		timer = ros::Time::now();
+	}
+	ros::Duration count_down = ros::Time::now() - timer;
+	if (count_down.toSec() >= 5.0) {
+		done = true;
+	}
+}
+
+void dest_action_wait(bool& started, bool& done){
+	static ros::Time timer;
+	if(!started && !done){
+		started = true;
+		timer = ros::Time::now();
+	}
+	ros::Duration count_down = ros::Time::now() - timer;
+	if (count_down.toSec() >= 1.0){
+		done = true;
+	}
+}
+
 int main(int argc, char** argv){
-	ros::init(argc, argv, "map_view");
+	ros::init(argc, argv, "IEEE_map");
 	ros::NodeHandle nh;
 	ros::Rate rate(30);
 
-	ieee_map.init_visualization(&nh, "0");
-	ieee_caution.init_visualization(&nh, "1");
+	ros::Subscriber bot_sub = nh.subscribe("IMU_sensor", 10, updateBotCallback);
 
-	ieee_caution.setFieldWidth(0.25);
-	ieee_caution.setContainersWidth(0.25);
-	ieee_caution.setBotWidth(0.25);
-	ieee_map.setFieldColor(0, 0, 1.0);
-	ieee_map.setContainersColor(1.0, 0, 1.0);
-	ieee_map.setBotColor(0, 1.0, 0);
+	//tf::TransformListener listener;
+	//tf::StampedTransform sensor_transform;
+
+//	ieee_map.init(&nh, "0", false);
+	ieee_caution.init(&nh, "1", true);
+
 	geometry_msgs::Vector3 box_size;
 	box_size.x = 4;
 	box_size.y = 4;
 	box_size.z = 1;
-	ieee_map.createBotController(box_size, 6);
+	ieee_caution.createBotController(box_size, 6);
+
+	box_size.x = 2.5;
+	box_size.y = 2.5;
+	ieee_caution.createContainersController(box_size, 3.5);
+
+	unsigned int pad_num = 0;
+	geometry_msgs::Pose bot_pose = ieee_caution.original_bot_pose;
+	ieee_caution.addCheckPoint(pad_num++, tf::Vector3(bot_pose.position.x, bot_pose.position.y, 0), 0, map::Map::doNothing, dest_action_0);
+	ieee_caution.addCheckPoint(pad_num++, tf::Vector3(bot_pose.position.x, 9, 0), 0, map::Map::doNothing, dest_action_wait);
+	ieee_caution.addCheckPoint(pad_num++, tf::Vector3(9, 9, 0), M_PI / 2.0, map::Map::doNothing, dest_action_wait);
+	ieee_caution.addCheckPoint(pad_num++, tf::Vector3(9, 36, 0), 0, map::Map::doNothing, dest_action_wait);
+	ieee_caution.addCheckPoint(pad_num++, tf::Vector3(15, 36, 0), -M_PI / 2.0, map::Map::doNothing, dest_action_wait);
+	ieee_caution.addCheckPoint(pad_num++, tf::Vector3(15, 22.5, 0), 0, map::Map::doNothing, dest_action_wait);
+	ieee_caution.addCheckPoint(pad_num++, tf::Vector3(83, 22.5, 0), 0, map::Map::doNothing, dest_action_wait);
+	ieee_caution.addCheckPoint(pad_num++, tf::Vector3(83, 22.5, 0), M_PI / 2.0, map::Map::doNothing, dest_action_wait);
+	ieee_caution.addCheckPoint(pad_num++, tf::Vector3(83, 36, 0), M_PI / 2.0, map::Map::doNothing, dest_action_wait);
+	ieee_caution.addCheckPoint(pad_num++, tf::Vector3(83, 36, 0), -M_PI / 2.0, map::Map::doNothing, dest_action_wait);
+	ieee_caution.addCheckPoint(pad_num++, tf::Vector3(83, 9, 0), -M_PI / 2.0, map::Map::doNothing, dest_action_wait);
+	ieee_caution.addCheckPoint(pad_num++, tf::Vector3(61.75 + 9, 22.5, 0), M_PI, map::Map::doNothing, dest_action_wait);
+	ieee_caution.addCheckPoint(pad_num++, tf::Vector3(55.25 - 9, 22.5, 0), M_PI, map::Map::doNothing, dest_action_wait);
+	ieee_caution.addCheckPoint(pad_num++, tf::Vector3(bot_pose.position.x, bot_pose.position.y, 0), M_PI / 2, map::Map::doNothing, dest_action_wait);
+	ieee_caution.publishField();
 
 	while(ros::ok()){
-		ieee_map.publish_field();
-		//ieee_map.publish_bot();
-		ieee_map.publish_containers();
-		ieee_caution.publish_all();
+
+		ieee_caution.checkCollision();
+		ieee_caution.followPath();
+		ieee_caution.pseudoMoveBot();
+/*
+		ros::Time now = ros::Time::now();
+
+		try{
+			listener.waitForTransform("/bot", "/sensor", now, ros::Duration(1.0));
+			listener.lookupTranform("/bot", "/sensor", now, transform);
+		}
+		catch (tf::TransformException &ex){
+			ROS_ERROR("%s", ex.what());
+			ros::Duration(1.0).sleep();
+			continue;
+		}
+*/
 		ros::spinOnce();
 		rate.sleep();
 	}
--- src/mining_map/include/mining_map/Map.h
+++ src/mining_map/include/mining_map/Map.h
@@ -5,8 +5,10 @@
 #include <interactive_markers/menu_handler.h>
 #include <visualization_msgs/MarkerArray.h>
 #include <visualization_msgs/Marker.h>
+#include <tf/transform_broadcaster.h>
 #include <geometry_msgs/Vector3.h>
 #include <geometry_msgs/Point.h>
+#include <std_msgs/ColorRGBA.h>
 #include <ros/ros.h>
 #include <tf/tf.h>
 #include <string>
@@ -16,6 +18,16 @@
 #define MAP_H
 
 namespace map{
+	#define H_PI				(M_PI / 2)
+	#define TO_DEGREE			(180.0 / M_PI)
+	#define BOT_MAX_LINEAR_VELOCITY 	(5)
+	#define BOT_MAX_ANGULAR_VELOCITY 	(0.8)
+	#define STOP_RADIUS 			(0.01)
+	#define STOP_ANGLE_P 			(0.01)
+	#define STOP_ANGLE_N 			(M_PI - 0.01)
+
+	typedef std::function<void(const visualization_msgs::InteractiveMarkerFeedbackConstPtr&)> ProcessFeedback;
+	typedef void (*Action)(bool&,bool&);
 
 	struct Segment{
 		geometry_msgs::Point p1;
@@ -26,13 +38,13 @@ namespace map{
 		int normal_direction;
 
 		Segment(geometry_msgs::Point, geometry_msgs::Point, int);
-		Segment place(float, float, float, float);
+		Segment place(double, double, double, double);
 		geometry_msgs::Point getIntersect();
 	};
 
-	struct Intersection{
-		float a;
-		float b;
+	struct Collision{
+		geometry_msgs::Point a;
+//		geometry_msgs::Point b;
 		Segment* seg1;
 		Segment* seg2;
 	};
@@ -42,60 +54,110 @@ namespace map{
 		Segment* oriented_walls[4];
 		geometry_msgs::Pose pose;
 
-		Box(float, float, float, float orientation=0);
-		void place(float, float, float);
+		Box(geometry_msgs::Pose, double);
+		void place(geometry_msgs::Pose);
+	};
+
+	void default_action(bool& started, bool& isDone){ started = true; isDone = true; } 
+
+	struct CheckPoint {
+		geometry_msgs::Pose destination;
+		bool action_done = false;
+		bool destination_action_done = false;
+		bool action_started = false;
+		bool destination_action_started = false;
+		bool arrived = false;
+		Action action;
+		Action destination_action;
 	};
 
 	class Map{
 		protected:
-			const uint8_t LEFT_WALL = 0;
-			const uint8_t TOP_WALL = 1;
-			const uint8_t RIGHT_WALL = 2;
-			const uint8_t BOTTOM_WALL = 3;
-			const uint8_t OUTER_BOTTOM_WALL = 4;
-			const uint8_t MID_BOTTOM_WALL = 5;
-			const uint8_t INNER_BOTTOM__WALL = 6;
-			const uint8_t OUTER_TOP_WALL = 7;
-			const uint8_t MID_TOP_WALL = 8;
-			const uint8_t INNER_TOP_WALL = 9;
+			const static uint8_t LEFT_WALL = 0;
+			const static uint8_t TOP_WALL = 1;
+			const static uint8_t RIGHT_WALL = 2;
+			const static uint8_t BOTTOM_WALL = 3;
+			const static uint8_t OUTER_BOTTOM_WALL = 4;
+			const static uint8_t MID_BOTTOM_WALL = 5;
+			const static uint8_t INNER_BOTTOM__WALL = 6;
+			const static uint8_t OUTER_TOP_WALL = 7;
+			const static uint8_t MID_TOP_WALL = 8;
+			const static uint8_t INNER_TOP_WALL = 9;
 			const static uint8_t WALL_COUNT = 14;
 			const static uint8_t CONTAINER_COUNT = 2;
+			const static uint8_t OBJECTS_COUNT = 4;
+			const static uint8_t FIELD = 0;
+			const static uint8_t CONTAINER1 = 1;
+			const static uint8_t CONTAINER2 = 2;
+			const static uint8_t BOT = 3;
 		private:
 			Segment* Walls[WALL_COUNT];
 			Box* Containers[CONTAINER_COUNT];
 			Box* Bot;
+			Box* Bot_zone;
 
-	                ros::Publisher marker_pub;
+			ros::Publisher marker_pub;
 	                ros::Publisher markerArray_pub;
+	                ros::Publisher bot_pub;
+	                ros::Publisher bot_vel_pub;
 
 			visualization_msgs::Marker field;
-                	visualization_msgs::MarkerArray container_markers;
+			visualization_msgs::Marker actual_field;
                 	visualization_msgs::Marker bot_marker;
-                	visualization_msgs::MarkerArray normal_vector_markers;
-		public:
+                	visualization_msgs::Marker bot_zone_marker;
+			visualization_msgs::Marker collision_point;
+			visualization_msgs::Marker path_arrow;
+			visualization_msgs::Marker path_line;
+                	visualization_msgs::MarkerArray container_markers;
+                	visualization_msgs::MarkerArray normal_vector_markers_list[OBJECTS_COUNT];
+			visualization_msgs::MarkerArray collision_points;
+			visualization_msgs::MarkerArray path_marker;
+
+			std::vector<Collision> collisions;
+			std::vector<CheckPoint> path;
+			std::vector<unsigned int> checkpoint_list;
+
 			static boost::shared_ptr<interactive_markers::InteractiveMarkerServer> server;
 			static interactive_markers::MenuHandler menu_handler;
+			bool debug_mode;
+			unsigned int current_checkpoint = 0;
+			bool run_path = false;
+
+			geometry_msgs::Vector3 bot_vel;
+
+		public:
+			geometry_msgs::Pose original_bot_pose;
+			geometry_msgs::Pose original_container0_pose;
+			geometry_msgs::Pose original_container1_pose;
 
 			Map(double);
-			static Intersection calculate_intersection(Segment*, Segment*);
-			void init_visualization(ros::NodeHandle*, std::string id="0");
-			void publish_bot();
-			void publish_containers();
-			void publish_field();
-			void publish_debugger();
-			void publish_all();
-			void setFieldColor(float, float, float, float a=1.0);
-			void setFieldWidth(float);
-			void setContainersColor(float, float, float, float a=1.0);
-			void setContainersWidth(float);
-			void setBotColor(float, float, float, float a=1.0);
-			void setBotWidth(float);
-			//void processFeedback(const visualization_msgs::InteractiveMarkerFeedbackConstPtr &feedback);
-			void makeBoxControl(visualization_msgs::Marker &marker, geometry_msgs::Pose, geometry_msgs::Vector3, float);
-			void createBotController(geometry_msgs::Vector3, float);
-			void createContainersController(geometry_msgs::Vector3, float);
+			void init(ros::NodeHandle*, std::string id="0", bool debug=false);
+			void subscribeBot(ros::NodeHandle*, std::string);
+			void publishField();
+			void setContainersColor(double, double, double, double a=1.0);
+			void setContainersWidth(double);
+			void setRunPath(bool);
+			void makeBoxControl(std::string con_name, geometry_msgs::Pose, geometry_msgs::Vector3, double, ProcessFeedback);
+			void createBotController(geometry_msgs::Vector3, double);
+			void createContainersController(geometry_msgs::Vector3, double);
+			void tfBroadcastBot(ros::NodeHandle*);
+			void updateBot(geometry_msgs::Pose);
+			void updateContainer(uint8_t, geometry_msgs::Pose);
+			void updateBotMarker();
+			void updateContainerMarkers();
+			void updatePathMarker();
+			void checkCollision();
+			void followPath();
+			bool moveBot(geometry_msgs::Pose);
+			void moveBotMarker(geometry_msgs::Pose);
+			void addCheckPoint(unsigned int, tf::Vector3, double, Action action=default_action, Action dest_action = default_action);
+			void removeCheckPoint(unsigned int);
+			void static startPath(const visualization_msgs::InteractiveMarkerFeedbackConstPtr&, Map*);
+			void pseudoMoveBot();
+			void static doNothing(bool&, bool&);
 	};
-	void processFeedback(const visualization_msgs::InteractiveMarkerFeedbackConstPtr &feedback);
+
+	void cal_intersection(Segment*, Segment*, std::vector<Collision>*);
 }
 
 #endif
